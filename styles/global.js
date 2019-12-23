6eafc0b484eee62213b82a2b158db1483f634ceb
Codemecodeyou <codemecodeyou@gmail.com>
2019-12-23 19:00:50 +0300

    added global styles

---
+++ styles/global.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    padding: 40
+  },
+  titleText: {
+    fontFamily: 'nunito-bold',
+    fontSize: 18,
+  },
+  paragraph: {
+    lineHeight: 20,
+    marginVertical: 8
+  }
+});
+
